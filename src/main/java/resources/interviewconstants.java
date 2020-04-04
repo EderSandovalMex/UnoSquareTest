3035d3352f08d2aa7f61adccde2057d6a8164792
EderSandovalMex <seder_roberto@hotmail.com>
2020-04-03 18:46:15 -0600

    getting ready for the interview

---
+++ src/main/java/resources/interviewconstants.java
@@ -0,0 +1,8 @@
+package resources;
+
+public class interviewconstants {
+
+	public static final String varOneInter =  "Print this please Constant 1";
+	public static final String vartwoInter =  "Print this please Constant 2";
+	public static final String varthreeInter = "Print this please Constant 3";
+}
--- src/test/java/unoSquareInterview/unoSquareTest.java
+++ src/test/java/unoSquareInterview/unoSquareTest.java
@@ -9,24 +9,29 @@ import org.testng.annotations.Test;
 
 import junit.framework.Assert;
 import resources.base;
+import resources.interviewconstants;
+ 
 
 public class unoSquareTest extends base{
 	
 	 
 	@Test 
-	public void UnosquareTest1() 
+	public void UnosquareTest1() throws IOException 
 	{
-		System.out.println("Dummy Method for the UnosquareTest");
+		driver = initializeDriver();
+		System.out.println(interviewconstants.varOneInter);
+		
 	}
 	@Test 
-	public void UnosquareTest2() 
+	public void UnosquareTest2() throws IOException 
 	{
-		System.out.println("Dummy Method for the UnosquareTest");
+		driver = initializeDriver();
+		System.out.println(interviewconstants.vartwoInter);
 	}
 	@Test 
 	public void UnosquareTest3() 
 	{
-		System.out.println("Dummy Method for the UnosquareTest");
+		System.out.println(interviewconstants.varthreeInter);
 	}
 	
 	
